825d24d29deb513a70131cba6edc7da5a32fead7
bshock84 <bshock84@yahoo.com>
2017-02-25 22:14:48 -0600

    Beginning of program.  Started working on layout of main screen.

---
+++ ConversationStarter/TopicDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  TopicDataModel.swift
+//  ConversationStarter
+//
+//  Created by Ben Shockley on 2/25/17.
+//  Copyright © 2017 Ben Shockley. All rights reserved.
+//
+
+import Foundation
+
+class Topics {
+    
+}
