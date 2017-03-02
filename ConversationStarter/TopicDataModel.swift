6d31d03913ad12792b455bd376bf9c97d8a55e8f
bshock84 <bshock84@yahoo.com>
2017-03-01 19:04:09 -0600

    started working on object models for the data.

--- ConversationStarter/TopicDataModel.swift
+++ ConversationStarter/TopicDataModel.swift
@@ -8,6 +8,51 @@
 
 import Foundation
 
-class Topics {
+//struct TopicObject {
+//    var topicCategory: String
+//    var topicsArray: [String]
+//}
+//
+//class Topics {
+//    
+//    var allTopics: [TopicObject] = [
+//                                    TopicObject.init(topicCategory: "Relationship Topics", topicsArray: [
+//                                        "What's your favorite position",
+//                                        "How many partners have you had?"
+//                                        ])
+//    ]
+//    
+//}
+
+
+enum TopicCategory: String {
+    case relationship = "Relationships"
+    case personalHistory = "Personal History"
+    case sex = "Sex"
+    case movies = "Movies"
+    case music = "Music"
+    case hobbies = "Hobbies"
+    case books = "Books"
+    case sports = "Sports"
+    case food = "Food"
+    case travel = "Travel"
+    case fasion = "Fasion"
+    case goals = "Goals"
+    case seasons = "Seasons"
+    case holidays = "Holidays"
+    case education = "Education"
+    case politics = "Politics"
+    case weird = "Weird"
+    case deep = "Deep"
+    case family = "Family"
+    case miscellaneous = "Miscellaneous"
     
+    
+    
+}
+
+struct TopicObject {
+    let topicCategory: TopicCategory
+    let topicText: String
+    let dontAskAgain: Bool = false
 }
